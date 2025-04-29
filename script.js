7c8aca2c1418e53102c47a380bd7d788ba9cb81a
Max-Jekov <maxzhekov07@gmail.com>
2025-04-29 21:50:42 +0300

    add HW

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-"use strict";
+const company = {
+  sales: [
+    { name: "John", salary: 1000 },
+    { name: "Alice", salary: 600 },
+  ],
+  development: {
+    web: [
+      { name: "Peter", salary: 2000 },
+      { name: "Alex", salary: 1800 },
+    ],
+    internals: [{ name: "Jack", salary: 1300 }],
+  },
+};
+
+const getAllSallary = (obj) => {
+  let allSallary = 0;
+
+  if (Array.isArray(obj)) {
+    obj.forEach((element) => {
+      allSallary += getAllSallary(element);
+    });
+  } else {
+    if (obj.hasOwnProperty("salary")) {
+      allSallary += obj.salary;
+    } else {
+      for (const key in obj) {
+        allSallary += getAllSallary(obj[key]);
+      }
+    }
+  }
+  return allSallary;
+};
+
+console.log(getAllSallary(company));
