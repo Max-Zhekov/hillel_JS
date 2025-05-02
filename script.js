c995b1212ef5faf19e1fffd880e124cc3ac9712f
Max-Jekov <maxzhekov07@gmail.com>
2025-05-02 21:54:45 +0300

    add HW

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 "use strict";
+const contactBook = {
+  contacts: [
+    {
+      name: "Olena",
+      phone: "+380631112233",
+      email: "olena@example.com",
+    },
+    {
+      name: "Andriy",
+      phone: "+380501234567",
+      email: "andriy@example.com",
+    },
+  ],
+  findContact(name) {
+    const contact = this.contacts.find((element) => element.name === name);
+    return contact ? contact : "User not found";
+  },
+  addContact(userName, userPhone, userEmail) {
+    this.contacts.push({ name: userName, phone: userPhone, email: userEmail });
+    return this.contacts;
+  },
+};
+
+console.log(contactBook.findContact("Andriy"));
+console.log(contactBook.findContact("Dima"));
+console.log(contactBook.findContact("Olena"));
+console.log(contactBook.findContact(123));
+
+console.log(contactBook.addContact("Dima", "+380000000", "dima@gmail.com"));
