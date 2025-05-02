ac2d0b790c460289fd2f8eae0c83189889610edc
Max-Jekov <maxzhekov07@gmail.com>
2025-05-02 21:30:17 +0300

    add HW

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use strict";
+const numbers = [];
+for (let i = 0; i < 100; i++) {
+  numbers.push(Math.floor(Math.random() * 1000));
+}
+const evenNumbers = [];
+numbers.forEach((item) => (item % 2 === 0 ? evenNumbers.push(item) : null));
+console.log(evenNumbers);
