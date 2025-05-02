3fe8b7cfbad07d43c77deec7bfec07d063ceccc4
Max-Jekov <maxzhekov07@gmail.com>
2025-05-02 21:22:56 +0300

    add HW

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+const user = {
+  userName: "Idris",
+  userAge: 21,
+  userLocation: "Poland",
+  userInfo() {
+    console.log(
+      `User name - ${this.userName}, user age - ${this.userAge}, user location - ${this.userLocation}`
+    );
+  },
+};
+console.log(user.userInfo());
