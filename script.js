09869480428cab8a05f40ab31c536fe2b0792d08
Max-Jekov <maxzhekov07@gmail.com>
2025-05-05 21:47:42 +0300

    add HW

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const text = document.querySelector("#text");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function () {
+  text.classList.toggle("red-color");
+});
