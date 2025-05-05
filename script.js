b3925fb9cf6b5bc2d6765a188236dba5df5a9547
Max-Jekov <maxzhekov07@gmail.com>
2025-05-05 21:53:22 +0300

    add HW

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 "use strict";
-
-const text = document.querySelector("#text");
-const btn = document.querySelector("#btn");
-
-btn.addEventListener("click", function () {
-  text.classList.toggle("red-color");
-});
