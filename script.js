a1ca5f4d20908b0a86ccc7b7fbf96140e4da6cb3
Max-Jekov <maxzhekov07@gmail.com>
2025-05-05 22:09:24 +0300

    add HW

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use strict";
+const photoEL = document.querySelector(".photo");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function () {
+  const photoNumber = Math.trunc(Math.random() * 9) + 1;
+  photoEL.src = `./img/${photoNumber}.jpg`;
+});
