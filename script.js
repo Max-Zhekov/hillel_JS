fd251a63db81d7f915b56d651e9bdee21c3ec84f
Max-Jekov <maxzhekov07@gmail.com>
2025-05-06 17:10:50 +0300

    add HW

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+const table = document.createElement("table");
+table.id = "table";
+
+for (let i = 1; i <= 10; i++) {
+  const row = document.createElement("tr");
+  for (let j = 1; j <= 10; j++) {
+    const cell = document.createElement("td");
+    cell.textContent = i * j;
+    row.appendChild(cell);
+  }
+  table.appendChild(row);
+}
+document.body.appendChild(table);
