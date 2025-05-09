b6719aa303fbcc2186fa5bd07680abd750b9e2d7
Max-Jekov <maxzhekov07@gmail.com>
2025-05-09 22:35:30 +0300

    add HW

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const enter = document.querySelector("#enter");
+const goTo = document.querySelector("#goTo");
+let address = "";
+
+enter.addEventListener("click", () => {
+  const input = prompt("Enter address (e.g., https://example.com):");
+  if (input) {
+    address = input;
+  }
+});
+
+goTo.addEventListener("click", () => {
+  if (address) {
+    location.assign(address);
+  } else {
+    alert("No address entered yet.");
+  }
+});
