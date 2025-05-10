702b39895c917e3618c96033205d2ae8cd5e0b6f
Max-Jekov <maxzhekov07@gmail.com>
2025-05-10 17:31:11 +0300

    add HW

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const container = document.querySelector(".container");
+container.addEventListener("click", function (event) {
+  if (event.target.tagName === "BUTTON") {
+    const buttonName = event.target.dataset.name;
+    alert(`Клікнуто на кнопці: ${buttonName}`);
+  }
+});
