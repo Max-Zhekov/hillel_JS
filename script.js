fe3febea927790fbc4b47351a0dfc1126d60fa14
Max-Jekov <maxzhekov07@gmail.com>
2025-05-10 18:00:24 +0300

    add HW

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const taskList = document.getElementById("taskList");
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+
+addBtn.addEventListener("click", function () {
+  const taskText = taskInput.value.trim();
+  if (!taskText) return;
+
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <span>${taskText}</span>
+    <button class="btn-delete">Видалити</button>
+  `;
+
+  taskList.appendChild(li);
+  taskInput.value = "";
+});
+
+taskList.addEventListener("click", function (event) {
+  if (event.target.classList.contains("btn-delete")) {
+    event.target.closest("li").remove();
+  }
+});
