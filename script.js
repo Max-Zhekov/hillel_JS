caabd58109114177a9a8cc2f136bee1228b647e9
Max-Jekov <maxzhekov07@gmail.com>
2025-05-31 11:22:17 +0300

    Added HW

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+const p = document.querySelector("p");
+let s = 5;
+
+function sToStr() {
+  if (s > 0) {
+    s = s - 1;
+
+    let m = Math.trunc(s / 60) + "";
+    let sec = (s % 60) + "";
+
+    p.textContent = String(m).padStart(2, 0) + ":" + String(sec).padStart(2, 0);
+  }
+}
+
+setInterval(sToStr, 1000);
