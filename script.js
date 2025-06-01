d33e65f5b0b60c2436e97d7cd50a8e40172dfbe1
Max-Jekov <maxzhekov07@gmail.com>
2025-06-01 14:36:57 +0300

    fix HW

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 "use strict";
 const p = document.querySelector("p");
-let s = 5;
+let secondsRemaining = 15;
+
+const startTimer = setInterval(sToStr, 1000);
 
 function sToStr() {
-  if (s > 0) {
-    s = s - 1;
+  if (secondsRemaining > 0) {
+    secondsRemaining = secondsRemaining - 1;
 
-    let m = Math.trunc(s / 60) + "";
-    let sec = (s % 60) + "";
+    let minutes = Math.trunc(secondsRemaining / 60) + "";
+    let secondsToEnd = (secondsRemaining % 60) + "";
 
-    p.textContent = String(m).padStart(2, 0) + ":" + String(sec).padStart(2, 0);
+    p.textContent =
+      String(minutes).padStart(2, 0) +
+      ":" +
+      String(secondsToEnd).padStart(2, 0);
+  } else {
+    clearInterval(startTimer);
   }
 }
-
-setInterval(sToStr, 1000);
